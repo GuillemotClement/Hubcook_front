998f284dea58b446db8255bd1744ee1e3d45638e
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-09 21:54:38 +0200

    Feat: add login and logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,47 @@
-import { NavLink, Link } from "react-router";
+import { NavLink, Link, useNavigate } from "react-router";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      const response = await fetch("http://localhost:8086/logout", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        throw new Error("Erreur serveur");
+      }
+
+      const dataResponse = await response.json();
+
+      console.log(dataResponse);
+      navigate("/");
+    } catch (err) {
+      console.error("Erreur :", err);
+    }
+  };
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
-    <header className="flex justify-between items-center shadow py-3 px-8">
-      <Link to="/" className="">
+    <header className='flex justify-between items-center shadow py-3 px-8'>
+      <Link to='/' className=''>
         <span>Hubcook</span>
       </Link>
-      <nav className="flex gap-2">
-        <NavLink to="/">Accueil</NavLink>
+      <nav className='flex gap-2'>
+        <NavLink to='/'>Accueil</NavLink>
       </nav>
-      <nav className="flex gap-2">
-        <NavLink to="/register" className="">
+      <nav className='flex gap-2'>
+        <NavLink to='/register' className=''>
           Inscription
         </NavLink>
-        <NavLink to="/login">Connexion</NavLink>
-        <NavLink to="/profil">Profil</NavLink>
-        <NavLink to="/logout">Deconnexion</NavLink>
+        <NavLink to='/login'>Connexion</NavLink>
+        <NavLink to='/profil'>Profil</NavLink>
+        <p onClick={handleLogout}>Deconnexion</p>
       </nav>
     </header>
   );
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,23 +1,72 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+
+const schema = yup.object({
+  username: yup.string().required("Une pseudo est necessaire"),
+  password: yup.string().required("Un mot de passe est requis"),
+});
+
+type FormData = {
+  username: string;
+  password: string;
+};
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+
+  const sendCredentials = async (data: FormData) => {
+    try {
+      const response = await fetch("http://localhost:8086/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        throw new Error("Erreur serveur");
+      }
+      const responseData = await response.json();
+      console.log(responseData);
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
+
+  const onSubmit = async (data: FormData) => {
+    await sendCredentials(data);
+  };
+
   return (
-    <div className="m-auto w-full">
-      <form action="" className="border py-3 px-2 m-auto w-96">
-        <h1 className="">Login Page</h1>
-        <div className="flex flex-col my-2">
-          <label htmlFor="">Username</label>
-          <input type="text" className="input-text shadow" />
+    <div className='m-auto w-full'>
+      <form action='' className='border py-3 px-2 m-auto w-96' onSubmit={handleSubmit(onSubmit)}>
+        <h1 className='text-center font-bold text-xl'>Login Page</h1>
+        <div className='flex flex-col my-2'>
+          <label htmlFor=''>Username</label>
+          <input type='text' className='input-text shadow' {...register("username")} />
+          <p className='text-red-500 font-bold'>{errors.username?.message}</p>
         </div>
-        <div className="flex flex-col my-2">
-          <label htmlFor="">Mot de passe</label>
-          <input type="text" name="" id="" className="input-text shadow" />
+        <div className='flex flex-col my-2'>
+          <label htmlFor=''>Mot de passe</label>
+          <input type='password' className='input-text shadow' {...register("password")} />
+          <p className='text-red-500 font-bold'>{errors.password?.message}</p>
         </div>
-        <div className="flex justify-between px-22">
-          <Link to="/" className="btn btn-neutral">
+        <div className='flex justify-between px-22'>
+          <Link to='/' className='btn btn-neutral'>
             Retour
           </Link>
-          <button type="button" className="btn  btn-primary">
+          <button type='submit' className='btn  btn-primary'>
             Connexion
           </button>
         </div>
